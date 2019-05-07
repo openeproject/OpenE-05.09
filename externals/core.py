ee112bc75c7da5cd5dcd417b9fd76867dad65f2a
Anthony Provenza <anthony-silva@hotmail.co.uk>
2019-05-07 19:06:45 +0100

    Fixed take/drop function
    Fixed the issue with take and drop, as they weren't working as expected.
    Also made some other small changes:
    1. Silenced a PyCharm warning
    2. Added the return type Token() to all of the functions
    3. Changed default parameter value from `[]` to `None`
    4. Removed the output of a matrix, when calling `add_row_top` as it was originally just for debugging
    5. Added More functions, that weren't ready for the last release

--- externals/core.py
+++ externals/core.py
@@ -9,24 +9,45 @@ from lib.opene.devkit import ExternalModule
 from lib.opene.devkit import DeveloperKit
 
 
+# noinspection PyUnusedLocal
 class Functions:
     @staticmethod
-    def hello_world(parameters: list = []):
+    def hello_world(parameters: list = None) -> Token:
+        """
+        Returns a simple string, good for testing
+        :param parameters:
+        :return:
+        """
         return Token("string", "\"Hello World! - From OpenE\"")
 
     @staticmethod
-    def test(parameters: list = []):
-        return Token("string","\"The external module 'core' was imported successfully\"")
+    def test(parameters: list = None) -> Token:
+        """
+        Returns a simple string. Good for testing
+        :param parameters:
+        :return:
+        """
+        return Token("string", "\"The external module 'core' was imported successfully\"")
 
     @staticmethod
-    def print(parameters: list = []):
+    def print(parameters: list = None) -> Token:
+        """
+        Ouputs the values given
+        :param parameters:
+        :return:
+        """
         for parameter in parameters:
             print(DeveloperKit().value_of(parameter), end="")
         print()
         return Token("Empty", "Empty")
 
     @staticmethod
-    def input(parameters: list = []):
+    def input(parameters: list = None) -> Token:
+        """
+        Gets the user to input a value. Uses the parameters as a prompt, similar to the print function
+        :param parameters:
+        :return:
+        """
         prompt = ""
         for parameter in parameters:
             prompt += DeveloperKit().value_of(parameter)
@@ -34,28 +55,146 @@ class Functions:
         return Token("string", "\"{}\"".format(user_input))
 
     @staticmethod
-    def range(parameters: list = []):
+    def range(parameters: list = None) -> Token:
+        """
+        Generates a list between 2 values
+        :param parameters:
+        :return:
+        """
         start_value = 0
         end_value = 0
         step = 1
         values = []
-        if (len(parameters) == 1):
-            end_value = int(parameters[0].value)
-        elif (len(parameters) == 2):
-            start_value = int(parameters[0].value)
-            end_value = int(parameters[1].value)
-        elif (len(parameters) == 3):
-            start_value = int(parameters[0].value)
-            end_value = int(parameters[1].value)
-            step = int(parameters[2].value)
-        else:
+        try:
+            if (len(parameters) == 1):
+                end_value = int(parameters[0].value)
+            elif (len(parameters) == 2):
+                start_value = int(parameters[0].value)
+                end_value = int(parameters[1].value)
+            elif (len(parameters) == 3):
+                start_value = int(parameters[0].value)
+                end_value = int(parameters[1].value)
+                step = int(parameters[2].value)
+            else:
+                pass  # Todo: Raise error
+            for value in range(start_value, end_value, step):
+                values.append(Token("integer", str(value)))
+            return Token("list", values)
+        except ValueError:
             pass  # Todo: Raise error
-        for value in range(start_value, end_value, step ):
-            values.append(Token("integer", str(value)))
-        return Token("list", values)
 
     @staticmethod
-    def to_matrix(parameters: list = []):
+    def first(parameters: list = None) -> Token:
+        """
+        Gets the first value of a list
+        :param parameters:
+        :return:
+        """
+        return parameters[0].value[0]
+
+    @staticmethod
+    def last(parameters: list = None) -> Token:
+        """
+        Gets the last value of a list
+        :param parameters:
+        :return:
+        """
+        return parameters[0].value[-1]
+
+    @staticmethod
+    def drop(parameters: list = None) -> Token:
+        """
+        Drops the first/last items of a list
+        - Positive values are the first items of a list
+        - Negative values are the last items of a list
+        :param parameters:
+        :return:
+        """
+        list_data = parameters[0].value
+        drop_value = int(DeveloperKit.value_of(parameters[1]))
+        if (drop_value > 0):
+            new_list = list_data[drop_value:]
+        elif (drop_value < 0):
+            new_list = list_data[:drop_value]
+        else:
+            new_list = list_data
+        return Token("list", new_list)
+
+    @staticmethod
+    def take(parameters: list = None) -> Token:
+        """
+        Takes the first/last items of a list
+        - Positive values are the first items of a list
+        - Negative values are the last items of a list
+        :param parameters:
+        :return:
+        """
+        list_data = parameters[0].value
+        take_value = int(DeveloperKit.value_of(parameters[1]))
+        if (take_value > 0):
+            new_list = list_data[:take_value]
+        elif (take_value < 0):
+            new_list = list_data[take_value:]
+        else:
+            new_list = list_data
+        return Token("list", new_list)
+
+    @staticmethod
+    def single(parameters: list = None) -> Token:
+        """
+        Takes a single item from a list, with the specififed position
+        :param parameters:
+        :return:
+        """
+        list_data = parameters[0].value
+        data_index = int(DeveloperKit.value_of(parameters[1]))
+        return list_data[data_index]
+
+    @staticmethod
+    def append(parameters: list = None) -> Token:
+        """
+        Adds the values to the end of the list
+        :param parameters:
+        :return:
+        """
+        list_data = parameters[0].value
+        for item in parameters[1:]:
+            list_data.append(item)
+        return Token("list", list_data)
+
+    @staticmethod
+    def remove(parameters: list = None) -> Token:
+        """
+        Removes an item based on its value
+        :param parameters:
+        :return:
+        """
+        list_data = parameters[0].value
+        value_to_remove = parameters[1]
+        new_list = list()
+        for item in list_data:
+            if (item.value != value_to_remove.value):
+                new_list.append(item)
+        return Token("list", new_list)
+
+    @staticmethod
+    def join(parameters: list = None) -> Token:
+        """
+        Joins together 1 or more lists
+        :param parameters:
+        :return:
+        """
+        final_list = list()
+        for current_list in parameters:
+            if (current_list.type == "list"):
+                for list_item in current_list.value:
+                    final_list.append(list_item)
+            else:
+                pass  # Todo: Raise error
+        return Token("list", final_list)
+
+    @staticmethod
+    def to_matrix(parameters: list = None) -> Token:
         if (parameters[0].type == "matrix"):
             matrix = parameters[0].value
             new_value = parameters[1]
@@ -75,7 +214,7 @@ class Functions:
             pass  # Todo: Raise error
 
     @staticmethod
-    def to_column(parameters: list = []):
+    def to_column(parameters: list = None) -> Token:
         if (parameters[0].type == "matrix"):
             matrix = parameters[0].value
             if (parameters[1].type == "list"):
@@ -149,6 +288,11 @@ class Functions:
 
     @staticmethod
     def remove_items(parameters: list = None) -> Token:
+        """
+        Removes the first x items from a list
+        :param parameters:
+        :return:
+        """
         if (parameters[0].type == "list" and parameters[1].type in ["sum", "integer", "float"]):
             old_list = parameters[0].value
             strip_value = int(eval(parameters[1].value))
@@ -169,17 +313,14 @@ class Functions:
             pass  # Todo: Raise error
 
     @staticmethod
-    def add_row_top(parameters: list = None) -> None:
+    def add_row_top(parameters: list = None) -> Token:
         matrix = parameters[0].value
         headers = parameters[1].value
-        new_matrix = []
+        new_matrix = list()
         new_matrix.append(headers)
         for row in matrix:
             new_matrix.append(row)
-        print(new_matrix)
         return Token("matrix", new_matrix)
-
-
 
 
 class ExternalParser(ExternalModule):
@@ -189,6 +330,16 @@ class ExternalParser(ExternalModule):
         "print": ["1=+", Functions.print],
         "input": ["1=+", Functions.input],
         "range": ["1=+", Functions.range],
+
+        "first": [1, Functions.first],
+        "last": [1, Functions.last],
+        "drop": [2, Functions.drop],
+        "take": ["2=+", Functions.take],
+        "single": [2, Functions.single],
+        "append": ["2=+", Functions.append],
+        "remove": ["2=+", Functions.remove],
+        "join": ["1=+", Functions.join],
+
         "to_matrix": [2, Functions.to_matrix],
         "to_column": [3, Functions.to_column],
         "get_column": [2, Functions.get_column],
